59d1ef64253663588fe305120bfa4297488c3078
tanipriya <tanisha.priya007@gmail.com>
2023-07-29 17:54:09 +0530

    create file for portfolio design by using reactjs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Portfolio/Navbar';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+    <div className="mx-auto max-w-7xl">
+      <Navbar/>
+    </div>
+  </div>
+  );
+}
+
+export default App;
