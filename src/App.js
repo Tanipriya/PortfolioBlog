e73c6bcc1540c5ed70d913f9ca5725ff10a59afb
tanipriya <tanisha.priya007@gmail.com>
2023-09-22 17:59:14 +0530

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Home } from 'lucide-react';
 import './App.css';
 import Navbar from './Portfolio/Navbar';
 
 function App() {
   return (
+    <>
     <div className="min-h-screen bg-gray-100">
     <div className="mx-auto max-w-7xl">
       <Navbar/>
     </div>
   </div>
+  </>
   );
 }
 
---
+++ src/Portfolio/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const home = () => {
+  return (
+    <>
+        <div className=' min-h-full bg-#0D2438'>
+            <Navbar/>
+        </div>
+    </>
+  )
+}
+
+export default home
